a0d599ee36e45f9a8f503a366bd7343bd6109796
marktex <marktex@outlook.com>
2020-09-25 21:12:42 +0800

    refactor(rules): 设置 rules 默认导出

--- src/rules/hr.js
+++ src/rules/hr.js
@@ -2,27 +2,29 @@ import reg from "./regexp";
 
 export function hr(state) {
     //  hr: /^(?:([\-] ?)+)\1\1$/gm,
-    const value = reg.hr.exec(state.src);
+    state.tokenize(reg.hr);
 
-    let raw = value[0],
-        index = value["index"],
-        input = value["input"],
-        map = [
-            index, // start index
-            index + raw.length - 1, // end index
-            input.substring(0, index).split(/\n/).length, // line number
-        ];
+    // const value = reg.hr.exec(state.src);
 
-    if (value) {
-        let l = state.tokens.push({
-            type: "hr",
-            value: [...value],
-            index,
-            raw,
-            map,
-        });
-        state.table[index] = l - 1;
-    }
+    // let raw = value[0],
+    //     index = value["index"],
+    //     input = value["input"],
+    //     map = [
+    //         index, // start index
+    //         index + raw.length - 1, // end index
+    //         input.substring(0, index).split(/\n/).length, // line number
+    //     ];
+
+    // if (value) {
+    //     let l = state.tokens.push({
+    //         type: "hr",
+    //         value: [...value],
+    //         index,
+    //         raw,
+    //         map,
+    //     });
+    //     state.table[index] = l - 1;
+    // }
 }
 
 export default hr;
---
+++ src/rules/index.js
@@ -0,0 +1,13 @@
+import hr from "./hr";
+import heading from "./heading";
+import list from "./list";
+
+export * from "./hr";
+export * from "./heading";
+export * from "./list";
+
+export default [
+    ["hr", hr],
+    ["heading", heading],
+    ["list", list],
+]
